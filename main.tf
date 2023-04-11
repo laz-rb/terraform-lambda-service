ae850888ca214e1d46caab0a8dc83c42a8548ed0
Eleazar Ponce <poncebe@gmail.com>
2023-04-11 15:15:31 -0600

    feat: Added API Gateway integration

--- main.tf
+++ main.tf
@@ -26,6 +26,15 @@ resource "aws_lambda_function" "this" {
   filename      = var.filename
 }
 
+resource "aws_lambda_permission" "this" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${var.apigateway_execution_arn}/*/*"
+}
+
 ############# CloudWatch #############
 resource "aws_cloudwatch_log_group" "this" {
   name = "/aws/lambda/${var.name}"
@@ -33,8 +42,23 @@ resource "aws_cloudwatch_log_group" "this" {
 }
 
 ############# API GateWay #############
-resource "aws_apigatewayv2_stage" "dev" {
-  name        = "dev"
-  api_id      = aws_apigatewayv2_api.this.id
+resource "aws_apigatewayv2_stage" "this" {
+  name        = var.name
+  api_id      = var.apigateway_api_id
   auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "this" {
+  api_id = var.apigateway_api_id
+
+  integration_uri    = aws_lambda_function.this.invoke_arn
+  integration_type   = var.apigatewayv2_integration_type
+  integration_method = var.apigateway_integration_method
+}
+
+resource "aws_apigatewayv2_route" "this" {
+  api_id = var.apigateway_api_id
+
+  route_key = var.apigateway_route_key
+  target    = "integrations/${aws_apigatewayv2_integration.this.id}"
 }
--- variables.tf
+++ variables.tf
@@ -24,3 +24,31 @@ variable "cloudwatch_log_group_retention" {
   description = "(Optional) Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1096, 1827, 2192, 2557, 2922, 3288, 3653, and 0. If you select 0, the events in the log group are always retained and never expire."
   default = 14
 }
+
+variable "apigateway_api_id" {
+  type = string
+  description = "API identifier."
+}
+
+variable "apigateway_execution_arn" {
+  type = string
+  description = "ARN prefix to be used in an aws_lambda_permission's source_arn attribute or in an aws_iam_policy to authorize access to the @connections API. See the Amazon [API Gateway Developer Guide](https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-websocket-control-access-iam.html) for details."
+}
+
+variable "apigateway_integration_type" {
+  type = string
+  description = "(Optional) Integration type of an integration. Valid values: AWS (supported only for WebSocket APIs), AWS_PROXY, HTTP (supported only for WebSocket APIs), HTTP_PROXY, MOCK (supported only for WebSocket APIs). For an HTTP API private integration, use HTTP_PROXY."
+  default = "AWS_PROXY"
+}
+
+variable "apigateway_integration_method" {
+  type = string
+  description = "(Optional) Integration's HTTP method. Must be specified if integration_type is not MOCK."
+  default = "POST"
+}
+
+variable "apigateway_route_key" {
+  type = string
+  description = "Route key for the route. For HTTP APIs, the route key can be a combination of an HTTP method and resource path, for example, GET /pets"
+}
+
