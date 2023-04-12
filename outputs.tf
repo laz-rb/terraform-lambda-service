6e76efc7dad6c1f58b5a978f1be9bcc1419130a2
Eleazar Ponce <poncebe@gmail.com>
2023-04-12 09:04:48 -0600

    chore: Renamed dns output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "apigateway_url" {
   value = join("", [aws_apigatewayv2_stage.this.invoke_url, var.apigateway_route_key_path])
 }
 
-output "custom_domain_url" {
+output "custom_dns_url" {
   value = join("", ["https://", aws_apigatewayv2_api_mapping.this.domain_name])
 }
